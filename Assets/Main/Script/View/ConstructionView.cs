0c13b1a21625e1cbcc8c337ce4c620ee794801e6
HTCTAIPEI\Paul_Hsin <Paul_Hsin@htc.com>
2019-06-14 18:34:09 +0800

    Add specific tower ui range

--- Assets/Main/Script/View/ConstructionView.cs
+++ Assets/Main/Script/View/ConstructionView.cs
@@ -10,6 +10,12 @@ public class ConstructionView : InGameUIBase
     [SerializeField]
     private GameObject ShopObject;
 
+    [SerializeField, Range(0,90)]
+    private float item_space = 15f;
+
+    [SerializeField, Range(0, 2)]
+    private float item_radius = 1.2f;
+
     private enum UILayout {
         EqualSpace,
         SpecificSpace
@@ -30,28 +36,60 @@ public class ConstructionView : InGameUIBase
         base.Show(isOn);
 
         if (isOn) {
-            ReLayoutUIComponent(UILayout.EqualSpace);
+            ReLayoutUIComponent(UILayout.SpecificSpace);
         }
     }
 
     private void ReLayoutUIComponent(UILayout ui_layout) {
         if (ShopObject != null) {
-            int componentLength = ShopObject.transform.childCount;
-            float space = 360f / componentLength;
-            int radius = 1;
 
-            for (int i = 0; i < componentLength; i++) {
-                Transform child = ShopObject.transform.GetChild(i);
+            switch (ui_layout) {
+                case UILayout.EqualSpace:
+                    ToEqualSpaceLayout(ShopObject.transform, item_radius);
+                break;
 
-                float angle = space * i;
-
-                if (componentLength % 2 == 0)
-                    angle += space / 2;
-
-                Vector3 rotatePos = MathUtility.AngleToVector3(angle);
-                child.position = ShopObject.transform.position + (rotatePos * radius);
+                case UILayout.SpecificSpace:
+                    ToSpecificLayout(ShopObject.transform, 0, item_radius, item_space);
+                break;
             }
+
         }
+    }
+
+    private void ToEqualSpaceLayout(Transform centerObject, float radius) {
+        int componentLength = centerObject.childCount;
+        float space = 360f / componentLength;
+
+        for (int i = 0; i < componentLength; i++)
+        {
+            Transform child = centerObject.GetChild(i);
+
+            float angle = space * i;
+
+            if (componentLength % 2 == 0)
+                angle += space / 2;
+
+            Vector3 rotatePos = MathUtility.AngleToVector3(angle);
+            child.position = centerObject.position + (rotatePos * radius);
+        }
+    }
+
+    private void ToSpecificLayout(Transform centerObject, float angle, float radius, float space) {
+        int componentLength = centerObject.childCount;
+
+
+        float totalSpace = (componentLength - 1) * space;
+
+        float startAngle = angle - (totalSpace / 2);
+        
+        for (int i = 0; i < componentLength; i++)
+        {
+            Transform child = centerObject.GetChild(i);
+
+            Vector3 rotatePos = MathUtility.AngleToVector3(startAngle + (space * i));
+            child.position = centerObject.position + (rotatePos * radius);
+        }
+
     }
 
 }
